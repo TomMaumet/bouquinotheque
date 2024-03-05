e15a824d3eb1ae0db62422eb632357c7fa4583a0
Thomas Maumet <thomasmaumet@free.fr>
2024-03-05 11:54:17 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
+  devise_for :users do
+    resources :playlists, only: [:new, :create]
+  end
+    resources :playlists, only: [:show, :index, :update, :destroy]
+  root to: "readings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+    resources :books, only: [:index, :show, :create] do
+      resources :readings, only: [:create]
+    end
+      resources :readings, only: [:index, :show, :update, :destroy] do
+        resources :reviews, only: [:create]
+      end
+      resources :book_stores
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
