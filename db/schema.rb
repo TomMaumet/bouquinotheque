bd2f5369048d399b88a5dac60789de4aa650bae9
Camille Haye <ca.haye@outlook.fr>
2024-03-05 17:05:16 +0100

    shema and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_145340) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_160252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_145340) do
     t.datetime "updated_at", null: false
     t.integer "publishing_year"
     t.string "EAN"
-    t.integer "book_type"
+    t.string "book_type"
     t.string "image_url"
   end
 
