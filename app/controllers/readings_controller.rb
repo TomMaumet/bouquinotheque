56981a36d653cba31c8bb70e414f50c648ea1f36
Thomas Maumet <thomasmaumet@free.fr>
2024-03-06 16:54:11 +0100

    readings

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,25 +1,7 @@
 class ReadingsController < ApplicationController
 
   def index
-    @readings = Reading.all
+    @readings = Reading.where(user: current_user)
   end
-
-  # def new
-  #   @book = Book.find(params[:book_id])
-  #   @reading = Reading.new
-  # end
-
-  # def create
-  #   @reading = Reading.new(reading_params)
-  #   @reading.book = @book
-  #   @reading.save
-  #   redirect_to reading_path(@readings)
-  # end
-
-  # private
-
-  # def reading_params
-  #   params.require(:reading).permit(:comment, :my_rating, :reading_status, :shared_to)
-  # end
 
 end
