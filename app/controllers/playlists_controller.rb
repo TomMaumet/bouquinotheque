0f0699663faadb0df08110ded0e35e895a44fe8d
bastien dubray <bastien.dubray@gmail.com>
2024-03-06 14:32:10 +0100

    Create begin

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,10 +2,22 @@ class PlaylistsController < ApplicationController
 
   def index
     @playlists = Playlist.all
+    @playlist_new = Playlist.new
   end
 
   def show
     @playlist = Playlist.find(params[:id])
     @readings = @playlist.readings
+  end
+
+  def create
+    @playlist_new = Playlit.new(set_playlist)
+    raise
+  end
+
+  private
+
+  def set_playlist
+    params.require(:playlist).permit(:title)
   end
 end
