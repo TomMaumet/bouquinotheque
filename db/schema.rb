f2cc6b61d3a429413ddbbaeb2bc5edcc5e00c3dc
bastien dubray <bastien.dubray@gmail.com>
2024-03-11 18:30:49 +0100

    Finish day

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_094453) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_161916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_094453) do
     t.string "EAN"
     t.string "book_type"
     t.string "image_url"
+    t.integer "thriller_score"
+    t.integer "romance_score"
+    t.integer "aventure_score"
+    t.integer "jeunesse_score"
   end
 
   create_table "friend_relationships", force: :cascade do |t|
@@ -113,6 +117,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_094453) do
     t.string "avatar"
     t.integer "age"
     t.string "city"
+    t.integer "thriller_score"
+    t.integer "romance_score"
+    t.integer "aventure_score"
+    t.integer "jeunesse_score"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
