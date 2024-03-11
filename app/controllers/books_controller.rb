69844a73087f1f0e59ca34b8e0e645c59b734bde
Thomas Maumet <thomasmaumet@free.fr>
2024-03-11 18:09:15 +0100

    API use

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "json"
+
 class BooksController < ApplicationController
 
   def index
@@ -12,7 +15,9 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    result = JSON.parse(URI.open("https://www.googleapis.com/books/v1/volumes?q=isbn:9782221252055").read)
+    title = result["items"][0]["volumeInfo"]["title"]
+    @book = Book.new(title: title)
   end
 
   def create
@@ -24,5 +29,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title)
   end
-
 end
