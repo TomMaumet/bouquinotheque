cd4d3e260ba83988b458be143d7b9af1a48654e0
Thomas Maumet <thomasmaumet@free.fr>
2024-03-12 13:07:33 +0100

    api call

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 require "open-uri"
 require "json"
+require "date"
 
 class BooksController < ApplicationController
 
@@ -15,24 +16,32 @@ class BooksController < ApplicationController
   end
 
   def new
-    result = JSON.parse(URI.open("https://www.googleapis.com/books/v1/volumes?q=isbn:9782014001334").read)
-    book_id = result["items"][0]["id"]
-    book_form = JSON.parse(URI.open("https://www.googleapis.com/books/v1/volumes/#{book_id}").read)
+    result = JSON.parse(URI.open("https://www.googleapis.com/books/v1/volumes?q=isbn:#{params[:EAN]}").read)
+    volume_id = result["items"][0]["id"]
+    book_form = JSON.parse(URI.open("https://www.googleapis.com/books/v1/volumes/#{volume_id}").read)
     title = book_form["volumeInfo"]["title"]
     author = book_form["volumeInfo"]["authors"]
     summary = book_form["volumeInfo"]["description"]
     publisher = book_form["volumeInfo"]["publisher"]
-    publishing_year = book_form["volumeInfo"]["publishedDate"]
-    @book = Book.new(title: title, author: author, summary: summary, publisher: publisher, publishing_year: publishing_year)
+    publishing_year = DateTime.parse(book_form["volumeInfo"]["publishedDate"]).year
+    genre = book_form["volumeInfo"]["categories"]
+    image_url = book_form["volumeInfo"]["imageLinks"]["thumbnail"]
+    @book = Book.new(title: title, author: author, summary: summary, publisher: publisher, publishing_year: publishing_year, genre: genre, EAN: params[:EAN], image_url: image_url)
   end
 
   def create
     @book = Book.new(book_params)
+    if @book.save
+      Reading.create(user: current_user, book: @book)
+      redirect_to readings_path(@readings)
+    else
+      redirect_to new_book_path(book_params)
+    end
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :author, :summary, :publisher, :publishing_year, :genre, :EAN, :image_url)
   end
 end
