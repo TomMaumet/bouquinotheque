bd8bfeffd648d38082e603513c91f717c19c9ea2
bastien dubray <bastien.dubray@gmail.com>
2024-03-12 10:23:36 +0100

    Add recommandation method

--- app/models/book.rb
+++ app/models/book.rb
@@ -20,18 +20,16 @@ class Book < ApplicationRecord
     vector_a = Vector[self.thriller_score, self.romance_score, self.aventure_score, self.jeunesse_score]
     #next, create a vector for the game being passed in to the method
     #we will then compare the two vectors to see how similar the games are
-    vector_b = Vector[self.thriller_score, self.romance_score, self.aventure_score, self.jeunesse_score]
+    vector_b = Vector[book.thriller_score, book.romance_score, book.aventure_score, book.jeunesse_score]
     #now that we have 2 vectors we will find the similarity by putting the dot product
     #in our numerator, and the product of each vectors magnitude in our denominator
-    raise
-    numerator = vector_a.inner_product  (vector_b)
+    numerator = vector_a.inner_product(vector_b)
     denominator = vector_a.r * vector_b.r
     #this will give the cosine similarity
     score = numerator / denominator * 100
     return score.to_i
     #since cosine is a decimal between 0 and 1, multiplying by 100 and then rounding to the nearest integer gives a nice scoring system from 0 to 100
   end
-
 
   private
 
